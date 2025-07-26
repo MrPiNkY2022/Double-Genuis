32524a7931b0405f83d3b1d6f49f5732f63d432b
MrPiNkY2022 <97333310+MrPiNkY2022@users.noreply.github.com>
2025-07-26 03:10:44 -0400

    Update DreamToken.sol

--- DreamToken.sol
+++ DreamToken.sol
@@ -1,1 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
 
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract DreamToken is ERC721URIStorage, Ownable {
+    uint256 public tokenId;
+
+    constructor() ERC721("DreamToken", "DREAM") {}
+
+    function mintDream(address to, string memory uri) external onlyOwner {
+        _mint(to, tokenId);
+        _setTokenURI(tokenId, uri);
+        tokenId++;
+    }
+}
